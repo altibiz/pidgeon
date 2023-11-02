b28a3550ce3d3d53553094ceaa52731b11c3ca99
Hrvoje Jurić <hrvoje@altibiz.com>
2023-11-02 15:55:32 +0000

    WIP

--- src/cli/src/modbus/conn.rs
+++ src/cli/src/modbus/conn.rs
@@ -10,61 +10,53 @@ use super::span::*;
 #[derive(Debug)]
 pub struct Connection {
   ctx: Context,
+}
+
+impl Connection {
+  pub async fn connect(socket: SocketAddr) -> Result<Self, std::io::Error> {
+    let stream = TcpStream::connect(socket).await?;
+    let ctx = tokio_modbus::prelude::tcp::attach(stream);
+    Ok(Self { ctx })
+  }
+
+  pub async fn connect_slave(
+    socket: SocketAddr,
+    slave: Slave,
+  ) -> Result<Self, std::io::Error> {
+    let stream = TcpStream::connect(socket).await?;
+    let ctx = tokio_modbus::prelude::rtu::attach_slave(stream, slave);
+    Ok(Self { ctx })
+  }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct ConnectionReadParams {
   timeout: futures_time::time::Duration,
   backoff: tokio::time::Duration,
   retries: usize,
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum ConnectError {
+pub enum ConnectionReadParamsError {
   #[error("Failed converting timeout")]
   TimeoutConversion(#[from] std::num::TryFromIntError),
 
   #[error("Failed converting backoff")]
-  BackoffConversion(#[from] chrono::OutOfRangeError),
-
-  #[error("Failed connecting to device")]
-  Connect(#[from] std::io::Error),
+  BackoffConversoin(#[from] chrono::OutOfRangeError),
 }
 
-impl Connection {
-  pub async fn connect(
-    socket: SocketAddr,
+impl ConnectionReadParams {
+  pub fn new(
     timeout: chrono::Duration,
     backoff: chrono::Duration,
     retries: usize,
-  ) -> Result<Self, ConnectError> {
+  ) -> Result<Self, ConnectionReadParamsError> {
     let timeout: futures_time::time::Duration =
       futures_time::time::Duration::from_millis(
         timeout.num_milliseconds().try_into()?,
       );
     let backoff: std::time::Duration = backoff.to_std()?;
-    let stream = TcpStream::connect(socket).await?;
-    let ctx = tokio_modbus::prelude::tcp::attach(stream);
     Ok(Self {
-      ctx,
-      timeout,
-      backoff,
-      retries,
-    })
-  }
-
-  pub async fn connect_slave(
-    socket: SocketAddr,
-    slave: Slave,
-    timeout: chrono::Duration,
-    backoff: chrono::Duration,
-    retries: usize,
-  ) -> Result<Self, ConnectError> {
-    let timeout: futures_time::time::Duration =
-      futures_time::time::Duration::from_millis(
-        timeout.num_milliseconds().try_into()?,
-      );
-    let backoff: std::time::Duration = backoff.to_std()?;
-    let stream = TcpStream::connect(socket).await?;
-    let ctx = tokio_modbus::prelude::rtu::attach_slave(stream, slave);
-    Ok(Self {
-      ctx,
       timeout,
       backoff,
       retries,
@@ -89,14 +81,15 @@ impl Connection {
   >(
     &mut self,
     spans: TIntoIterator,
+    params: ConnectionReadParams,
   ) -> Vec<Result<TSpan, ConnectionReadError>>
   where
     for<'a> &'a TIntoIterator: IntoIterator<Item = &'a TSpanParser>,
   {
     let mut results = Vec::new();
-    let backoff = self.backoff;
+    let backoff = params.backoff;
     for span in spans.into_iter() {
-      let parsed = self.read_span(span).await;
+      let parsed = self.read_span(span, params).await;
       results.push(parsed);
       tokio::time::sleep(backoff).await;
     }
@@ -106,6 +99,7 @@ impl Connection {
   pub async fn read_span<TSpan: Span, TSpanParser: SpanParser<TSpan>>(
     &mut self,
     register: &TSpanParser,
+    params: ConnectionReadParams,
   ) -> Result<TSpan, ConnectionReadError> {
     fn flatten_result<T, E1, E2>(
       result: Result<Result<T, E1>, E2>,
@@ -119,9 +113,9 @@ impl Connection {
     let data = {
       let address = register.address();
       let quantity = register.quantity();
-      let timeout = self.timeout;
-      let backoff = self.backoff;
-      let retries = self.retries;
+      let timeout = params.timeout;
+      let backoff = params.backoff;
+      let retries = params.retries;
       let mut retried = 0;
       let mut result = flatten_result(
         self
