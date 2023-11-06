9f5fdf53bf9cb85261e019083ee91ac7f3dbd414
Hrvoje Jurić <hrvoje@altibiz.com>
2023-11-06 12:03:16 +0100

    WIP

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Pidgeon - Raspberry Pi message broker.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.simpleFlake {
+    inherit self nixpkgs;
+    name = "pidgeon";
+    config = { allowUnfree = true; };
+    shell = { pkgs }: pkgs.mkShell {
+      packages = with pkgs; let
+        pyright = pkgs.writeShellApplication {
+          name = "pyright";
+          runtimeInputs = [ pkgs.poetry pkgs.nodePackages.pyright ];
+          text = ''
+            # shellcheck disable=SC1091
+            source "$(poetry env info --path)/bin/activate"
+            pyright-langserver "$@"
+          '';
+        };
+
+        python = pkgs.writeShellApplication {
+          name = "python";
+          runtimeInputs = [ pkgs.poetry ];
+          text = ''
+            poetry run python "$@"
+          '';
+        };
+      in
+      [
+        # Nix
+        nil
+        nixpkgs-fmt
+
+        # Python
+        poetry
+        pyright
+        python
+        (python310.withPackages
+          (pythonPackages: with pythonPackages; [
+            yapf
+          ]))
+
+        # Rust
+        llvmPackages.clangNoLibcxx
+        llvmPackages.lldb
+        rustc
+        cargo
+        clippy
+        rustfmt
+        rust-analyzer
+        cargo-edit
+        shfmt
+        marksman
+        taplo
+      ];
+    };
+  };
+}
