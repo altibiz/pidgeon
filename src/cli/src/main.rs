44ce7a06364ea6683c0761c87784c3cf727bb24b
Hrvoje Jurić <hrvoje@altibiz.com>
2023-11-11 19:15:44 +0100

    WIP

--- src/cli/src/main.rs
+++ src/cli/src/main.rs
@@ -18,3 +18,39 @@ mod service;
 async fn main() -> anyhow::Result<()> {
   Ok(())
 }
+
+struct Interval {
+  token: tokio_util::sync::CancellationToken,
+  handle: tokio::task::JoinHandle<()>,
+}
+
+macro_rules! interval {
+  ($rt:ident,$services:ident,$handler:ident,$duration:expr) => {{
+    let services = $services.clone();
+    let token = tokio_util::sync::CancellationToken::new();
+    let child_token = token.child_token();
+    let duration = $duration.clone();
+    let handle = $rt.r#async.spawn(async move {
+      let mut interval = tokio::time::interval(duration);
+
+      loop {
+        tokio::select! {
+            _ = child_token.cancelled() => {
+                return;
+            },
+            _ = async {
+                if let Err(error) = services.$handler().await {
+                    tracing::error! { %error, "interval handler failed" };
+                }
+
+                interval.tick().await;
+            } => {
+
+            }
+        }
+      }
+    });
+
+    Interval { token, handle }
+  }};
+}
