ff2cbc6da376a18c668be192870854328c3c4d06
Hrvoje Jurić <hrvoje@altibiz.com>
2023-11-16 14:22:49 +0100

    WIP

--- src/cli/src/process/discover.rs
+++ src/cli/src/process/discover.rs
@@ -196,7 +196,7 @@ impl Process {
       .read_from_destination(destination, device.id)
       .await;
 
-    dbg!(registers.iter());
+    tracing::debug!("REGISTERS {:?}", registers);
 
     let matched = registers.ok().map(|id_registers| DeviceMatch {
       kind: device.kind.clone(),
@@ -204,7 +204,7 @@ impl Process {
       id: modbus::make_id(device.kind, id_registers),
     });
 
-    dbg!(matched.clone());
+    tracing::debug!("MATCHED {:?}", matched);
 
     matched
   }
