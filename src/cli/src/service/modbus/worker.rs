2dc4415e373cfdc5bcc2b8e8106debf8e086fa09
Hrvoje Jurić <hrvoje@altibiz.com>
2023-11-21 02:59:10 +0100

    WIP

--- src/cli/src/service/modbus/worker.rs
+++ src/cli/src/service/modbus/worker.rs
@@ -11,8 +11,6 @@ use tokio::sync::Mutex;
 use super::connection::*;
 use super::span::{SimpleSpan, Span};
 
-// TODO: concurrent up to gateway/device max sockets
-
 // OPTIMIZE: remove copying when reading
 // OPTIMIZE: check bounded channel length - maybe config?
 
