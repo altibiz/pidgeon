21cc087d8ffd941e88be827ed83900866032cf62
Hrvoje Jurić <hrvoje@altibiz.com>
2023-11-23 12:00:17 +0000

    WIP

--- src/system/flake.nix
+++ src/system/flake.nix
@@ -58,19 +58,41 @@
           networking.hostName = "pidgeon";
 
           environment.systemPackages = with pkgs; [
+            # rpi dependencies
             libraspberrypi
             raspberrypi-eeprom
-            man-pages
-            man-pages-posix
-            openssl
+
+            # CLI dependencies
             pkg-config
+            openssl
+
+            # secrets
             age
             ssh-to-age
             sops
+
+            # sysadmin
+            man-pages
+            man-pages-posix
             kitty
-            git
             lazygit
+            git
             helix
+
+            # diag
+            lm_sensors # NOTE: get sensor information
+            dua # NOTE: get disk space usage interactively
+            duf # NOTE: disk space usage overview
+            du-dust # NOTE: disk space usage in a tree
+            pciutils # NOTE: lspci
+            lsof # NOTE: lsof -ni for ports
+            dmidecode # NOTE: sudo dmidecode for mobo info
+            inxi # NOTE: overall hardware info
+            hwinfo # NOTE: overall hardware info
+            htop # NOTE: CPU process manager
+            tokei # NOTE: count lines of code
+            dog # NOTE: dns client
+            upower # NOTE: battery power
           ];
           environment.sessionVariables = {
             PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
