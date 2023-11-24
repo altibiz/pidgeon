aee0a9d2d3f408c1588365f21ffa46ee30bf80af
Hrvoje Jurić <hrvoje@altibiz.com>
2023-11-24 02:10:57 +0100

    WIP

--- src/cli/src/service/modbus/worker.rs
+++ src/cli/src/service/modbus/worker.rs
@@ -11,9 +11,9 @@ use tokio::sync::Mutex;
 use super::connection::*;
 use super::span::{SimpleSpan, Span};
 
-// TODO: Read(Custom { kind: Other, error: ExceptionResponse { function: 3, exception: IllegalDataAddress } })
-// TODO: Read(Custom { kind: InvalidData, error: \"Invalid response header: expected/request = Header { transaction_id: 0, unit_id: 255 }, actual/response = Header { transaction_id: 0, unit_id: 2 }\" })
-// TODO: Timeout(Custom { kind: TimedOut, error: \"future timed out\" })
+// NOTE: Read(Custom { kind: Other, error: ExceptionResponse { function: 3, exception: IllegalDataAddress } })
+// NOTE: Read(Custom { kind: InvalidData, error: \"Invalid response header: expected/request = Header { transaction_id: 0, unit_id: 255 }, actual/response = Header { transaction_id: 0, unit_id: 2 }\" })
+// NOTE: Timeout(Custom { kind: TimedOut, error: \"future timed out\" })
 
 // OPTIMIZE: remove copying when reading
 // OPTIMIZE: check bounded channel length - maybe config?
