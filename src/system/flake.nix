650b09f395c747cf5950e67d558cb1bdb22eaa7d
Hrvoje Jurić <hrvoje@altibiz.com>
2024-01-09 14:42:57 +0000

    WIP

--- src/system/flake.nix
+++ src/system/flake.nix
@@ -197,42 +197,42 @@
         ({ pkgs, ... }: { })
 
         # visualization
-        {
-          services.grafana.enable = true;
-          services.grafana.settings = {
-            server = {
+        # {
+        #   services.grafana.enable = true;
+        #   services.grafana.settings = {
+        #     server = {
 
-              http_addr = "0.0.0.0";
-              http_port = 3000;
-            };
-            date_formats = {
-              default_timezone = "utc";
-            };
-          };
+        #       http_addr = "0.0.0.0";
+        #       http_port = 3000;
+        #     };
+        #     date_formats = {
+        #       default_timezone = "utc";
+        #     };
+        #   };
 
-          networking.firewall.allowedTCPPorts = [
-            3000
-          ];
+        #   networking.firewall.allowedTCPPorts = [
+        #     3000
+        #   ];
 
-          services.grafana.provision.enable = true;
-          services.grafana.provision.datasources.settings = {
-            apiVersion = 1;
-            datasources = [
-              {
-                name = "Pidgeon DB";
-                type = "postgres";
-                url = "localhost:5432";
-                user = "pidgeon";
-                jsonData = {
-                  database = "pidgeon";
-                  sslmode = "disable";
-                  version = 10; # NOTE: >=10
-                  timescaledb = true;
-                };
-              }
-            ];
-          };
-        }
+        #   services.grafana.provision.enable = true;
+        #   services.grafana.provision.datasources.settings = {
+        #     apiVersion = 1;
+        #     datasources = [
+        #       {
+        #         name = "Pidgeon DB";
+        #         type = "postgres";
+        #         url = "localhost:5432";
+        #         user = "pidgeon";
+        #         jsonData = {
+        #           database = "pidgeon";
+        #           sslmode = "disable";
+        #           version = 10; # NOTE: >=10
+        #           timescaledb = true;
+        #         };
+        #       }
+        #     ];
+        #   };
+        # }
 
         # maintenance
         {
