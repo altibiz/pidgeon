7d921fec570ce8bd3e212e08a819905bc86926e7
Hrvoje Jurić <hrvoje@altibiz.com>
2024-03-05 13:32:35 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -12,25 +12,25 @@
     config = { allowUnfree = true; };
     shell = { pkgs }: pkgs.mkShell {
       packages = with pkgs; let
-        pyright = pkgs.writeShellApplication {
-          name = "pyright-langserver";
-          runtimeInputs = [ pkgs.poetry pkgs.nodejs ];
-          text = ''
-            # shellcheck disable=SC1091
-            source "$(poetry env info --path)/bin/activate"
-            pyright-langserver "$@"
-          '';
-        };
-
-        yapf = pkgs.writeShellApplication {
-          name = "yapf";
-          runtimeInputs = [ pkgs.poetry ];
-          text = ''
-            # shellcheck disable=SC1091
-            source "$(poetry env info --path)/bin/activate"
-            yapf "$@"
-          '';
-        };
+        # NITPICK: these two dont work as id like to ...
+        # pyright = pkgs.writeShellApplication {
+        #   name = "pyright-langserver";
+        #   runtimeInputs = [ pkgs.poetry pkgs.nodejs ];
+        #   text = ''
+        #     # shellcheck disable=SC1091
+        #     source "$(poetry env info --path)/bin/activate"
+        #     pyright-langserver "$@"
+        #   '';
+        # };
+        # yapf = pkgs.writeShellApplication {
+        #   name = "yapf";
+        #   runtimeInputs = [ pkgs.poetry ];
+        #   text = ''
+        #     # shellcheck disable=SC1091
+        #     source "$(poetry env info --path)/bin/activate"
+        #     yapf "$@"
+        #   '';
+        # };
 
         # NITPICK: https://github.com/astral-sh/ruff/issues/1699
         # ruff = pkgs.writeShellApplication {
