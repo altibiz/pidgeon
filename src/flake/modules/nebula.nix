61fc9aea6ced91bc24532018d13fefe6d7accb8b
Hrvoje Jurić <hrvoje@altibiz.com>
2024-11-21 07:43:17 +0000

    WIP

--- src/flake/modules/nebula.nix
+++ src/flake/modules/nebula.nix
@@ -1,60 +1,58 @@
 { ... }:
 
 {
-  system = {
-    # NOTE: these values are not used but nix evaluates them for some reason
-    services.nebula.networks.ozds-vpn = {
-      enable = true;
-      cert = "/etc/nebula/host.crt";
-      key = "/etc/nebula/host.key";
-      ca = "/etc/nebula/ca.crt";
-      firewall.inbound = [
-        {
-          host = "all";
-          port = "all";
-          proto = "any";
-        }
+  # NOTE: these values are not used but nix evaluates them for some reason
+  services.nebula.networks.ozds-vpn = {
+    enable = true;
+    cert = "/etc/nebula/host.crt";
+    key = "/etc/nebula/host.key";
+    ca = "/etc/nebula/ca.crt";
+    firewall.inbound = [
+      {
+        host = "all";
+        port = "all";
+        proto = "any";
+      }
+    ];
+    firewall.outbound = [
+      {
+        host = "all";
+        port = "all";
+        proto = "any";
+      }
+    ];
+    lighthouses = [ "10.8.0.1" ];
+    staticHostMap = {
+      "ozds-vpn.altibiz.com" = [
+        "10.8.0.1:4242"
       ];
-      firewall.outbound = [
-        {
-          host = "all";
-          port = "all";
-          proto = "any";
-        }
-      ];
-      lighthouses = [ "10.8.0.1" ];
-      staticHostMap = {
-        "ozds-vpn.altibiz.com" = [
-          "10.8.0.1:4242"
-        ];
+    };
+    settings = {
+      handshakes = {
+        try_interval = "1s";
       };
-      settings = {
-        handshakes = {
-          try_interval = "1s";
-        };
-        static_map = {
-          cadence = "5m";
-          lookup_timeout = "10s";
-        };
+      static_map = {
+        cadence = "5m";
+        lookup_timeout = "10s";
       };
     };
-    sops.secrets."nebula.ca.pub" = {
-      path = "/etc/nebula/ca.crt";
-      owner = "nebula-ozds-vpn";
-      group = "nebula-ozds-vpn";
-      mode = "0644";
-    };
-    sops.secrets."nebula.crt.pub" = {
-      path = "/etc/nebula/host.crt";
-      owner = "nebula-ozds-vpn";
-      group = "nebula-ozds-vpn";
-      mode = "0644";
-    };
-    sops.secrets."nebula.crt" = {
-      path = "/etc/nebula/host.key";
-      owner = "nebula-ozds-vpn";
-      group = "nebula-ozds-vpn";
-      mode = "0400";
-    };
+  };
+  sops.secrets."nebula.ca.pub" = {
+    path = "/etc/nebula/ca.crt";
+    owner = "nebula-ozds-vpn";
+    group = "nebula-ozds-vpn";
+    mode = "0644";
+  };
+  sops.secrets."nebula.crt.pub" = {
+    path = "/etc/nebula/host.crt";
+    owner = "nebula-ozds-vpn";
+    group = "nebula-ozds-vpn";
+    mode = "0644";
+  };
+  sops.secrets."nebula.crt" = {
+    path = "/etc/nebula/host.key";
+    owner = "nebula-ozds-vpn";
+    group = "nebula-ozds-vpn";
+    mode = "0400";
   };
 }
