d2297ab1e8ec6b097af1f5fca5689d3595681d35
Hrvoje Jurić <hrvoje@altibiz.com>
2025-02-14 10:43:54 +0100

    WIP

--- src/flake/modules/secrets.nix
+++ src/flake/modules/secrets.nix
@@ -6,8 +6,10 @@
       sops-nix.nixosModules.default
     ];
 
-    sops.defaultSopsFile = "${self}/src/flake/configurations/${config.host}/secrets.yaml";
-    sops.age.keyFile = "/root/host.scrt.key";
+    config = {
+      sops.defaultSopsFile = "${self}/src/flake/configurations/${config.host}/secrets.yaml";
+      sops.age.keyFile = "/root/host.scrt.key";
+    };
   };
 
   home = {
@@ -15,7 +17,9 @@
       sops-nix.homeManagerModules.sops
     ];
 
-    sops.defaultSopsFile = "${self}/src/flake/configurations/${config.host}/secrets.yaml";
-    sops.age.keyFile = "/root/host.scrt.key";
+    config = {
+      sops.defaultSopsFile = "${self}/src/flake/configurations/${config.host}/secrets.yaml";
+      sops.age.keyFile = "/root/host.scrt.key";
+    };
   };
 }
