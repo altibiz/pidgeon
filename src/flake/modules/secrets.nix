1d19ca433b026fc5405939b95e6fe41fcb5b55a9
Hrvoje Jurić <hrvoje@altibiz.com>
2025-02-14 11:28:22 +0100

    WIP

--- src/flake/modules/secrets.nix
+++ src/flake/modules/secrets.nix
@@ -1,25 +1,18 @@
 { self, config, sops-nix, ... }:
 
 {
-  system = {
-    imports = [
-      sops-nix.nixosModules.default
-    ];
+  imports = [{
+    system = sops-nix.nixosModules.default;
+    home = sops-nix.homeManagerModules.sops;
+  }];
 
-    config = {
-      sops.defaultSopsFile = "${self}/src/flake/configurations/${config.host}/secrets.yaml";
-      sops.age.keyFile = "/root/host.scrt.key";
-    };
+  system = {
+    sops.defaultSopsFile = "${self}/src/flake/configurations/${config.host}/secrets.yaml";
+    sops.age.keyFile = "/root/host.scrt.key";
   };
 
   home = {
-    imports = [
-      sops-nix.homeManagerModules.sops
-    ];
-
-    config = {
-      sops.defaultSopsFile = "${self}/src/flake/configurations/${config.host}/secrets.yaml";
-      sops.age.keyFile = "/root/host.scrt.key";
-    };
+    sops.defaultSopsFile = "${self}/src/flake/configurations/${config.host}/secrets.yaml";
+    sops.age.keyFile = "/root/host.scrt.key";
   };
 }
