7d3eb67e1ad31cb1a799ca9a34976e744075d468
Hrvoje Jurić <hrvoje@altibiz.com>
2025-03-02 00:24:44 +0100

    WIP

--- src/flake/raspberryPi4/default.nix
+++ src/flake/raspberryPi4/default.nix
@@ -1,52 +1,7 @@
 { self, perch, lib, specialArgs, sops-nix, ... }:
 
 let
-  pidgeons = [
-    {
-      id = "grFvxoW5xQs69gfQfCSTIND8NT1L70tI";
-      ip = "10.8.0.12";
-    }
-    {
-      id = "TulPr4fOqSE82Ro6jyRSlEv92NFiN3fo";
-      ip = "10.8.0.11";
-    }
-    {
-      id = "PayZMy3Y0JIHJzwfwMRiH45NkXjAH7GT";
-      ip = "10.8.0.15";
-    }
-    {
-      id = "6k73Uam8eNOg0KXhVdXj3p1C7N5bMpqf";
-      ip = "10.8.0.10";
-    }
-    {
-      id = "EvztBTwG5Zryo7UJPZFjtYwMVuEqQyZ4";
-      ip = "10.8.0.16";
-    }
-    {
-      id = "K2iADfcQngJoZvssNxiZDvbErFq0w3jn";
-      ip = "10.8.0.17";
-    }
-    {
-      id = "HHeJNW843H35uc0gdh6sbJUftnLNklyj";
-      ip = "10.8.0.19";
-    }
-    {
-      id = "nEIcKYdZ8KG5Cm3qXTb0xhcSooccc6td";
-      ip = "10.8.0.14";
-    }
-    {
-      id = "8RHOU6R8p732mNDytEL9minBqdo2ax77";
-      ip = "10.8.0.13";
-    }
-    {
-      id = "n5t4DIM4zK2wB89NFKlfUJd2yAfoilFi";
-      ip = "10.8.0.20";
-    }
-    {
-      id = "shdJz2n5imgs87CefXClpi5DDpjbMKsT";
-      ip = "10.8.0.18";
-    }
-  ];
+  pidgeons = builtins.fromJSON ./pidgeons.json;
 in
 {
   seal.overlays.raspberryPi4 =
@@ -71,10 +26,15 @@ in
             system = "aarch64-linux";
             inherit specialArgs;
             modules = [
+              {
+                nixpkgs.overlays = [
+                  self.overlays.raspberryPi4
+                ];
+              }
               (perch.lib.import.importDirToFlatPathList ./.)
               sops-nix.nixosModules.default
               {
-                sops.defaultSopsFile = "${self}/src/flake/secrets/${pidgeon.id}.yaml";
+                sops.defaultSopsFile = ./secrets + "${pidgeon.id}.yaml";
                 sops.age.keyFile = "/root/host.scrt.key";
               }
             ];
