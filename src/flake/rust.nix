e2427dca397edbf5179f535e4d0174a600d05345
Hrvoje Jurić <hrvoje@altibiz.com>
2025-03-02 09:46:08 +0100

    WIP

--- src/flake/rust.nix
+++ src/flake/rust.nix
@@ -9,6 +9,14 @@
       naersk' = pkgs.callPackage naersk { };
     in
     naersk'.buildPackage {
+      name = "pidgeon-cli";
+      pname = "pidgeon-cli";
+      version = "0.1.0";
       src = self;
+      buildInputs = with pkgs; [
+        pkg-config
+        openssl
+        systemd
+      ];
     };
 }
