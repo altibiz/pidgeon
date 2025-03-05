c92d85321bc70446476d4bca34e4195db71ed99f
Hrvoje Jurić <hrvoje@altibiz.com>
2025-03-05 11:06:40 +0100

    WIP

--- scripts/flake/format.nix
+++ scripts/flake/format.nix
@@ -1,12 +1,16 @@
+{ self, ... }:
+
 {
   flake.lib.format.mkDevShell = pkgs: pkgs.mkShell {
+    inputsFrom = [
+      (self.lib.python.mkDevShell pkgs)
+    ];
     packages = with pkgs; [
       nodePackages.prettier
       just
       nixpkgs-fmt
       cargo
       rustfmt
-      yapf
       shfmt
     ];
   };
