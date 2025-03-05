cd3a51c21588d27ab9cae4ab1f650388c3a1c34a
Hrvoje Jurić <hrvoje@altibiz.com>
2025-03-05 11:13:13 +0100

    WIP

--- scripts/flake/docs.nix
+++ scripts/flake/docs.nix
@@ -5,18 +5,14 @@
     inputsFrom = [
       (self.lib.vcs.mkDevShell pkgs)
       (self.lib.scripts.mkDevShell pkgs)
+      (self.lib.rust.mkDevShell pkgs)
     ];
 
     packages = with pkgs; [
-      # documentation
       mdbook
       mdbook-plantuml
       plantuml
       openjdk
-
-      # rust
-      rustc
-      cargo
     ];
   };
 }
