e926906349a3113c23b1cfa73643c0f1cdef49ee
Hrvoje Jurić <hrvoje@altibiz.com>
2025-03-05 11:11:46 +0100

    WIP

--- scripts/flake/lint.nix
+++ scripts/flake/lint.nix
@@ -4,14 +4,13 @@
   flake.lib.lint.mkDevShell = pkgs: pkgs.mkShell {
     inputsFrom = [
       (self.lib.python.mkDevShell pkgs)
+      (self.lib.rust.mkDevShell pkgs)
     ];
     packages = with pkgs; [
       nodePackages.prettier
       nodePackages.cspell
       just
       nixpkgs-fmt
-      cargo
-      clippy
       shellcheck
     ];
   };
